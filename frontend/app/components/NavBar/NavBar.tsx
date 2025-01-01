a2741109a0e1703778e2dc8e964a0c8184767c3a
sharmaryan <panditaryan.as@gmail.com>
2025-01-01 22:28:46 +0530

    styles : navbar

---
+++ frontend/app/components/NavBar/NavBar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export const NavBar = () => {
+  return (
+    <header className="border-b border-gray-200 p-2">
+      <h1 className="text-2xl font-bold">Medium</h1>
+    </header>
+  );
+};
