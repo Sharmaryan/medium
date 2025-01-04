76e3fdfd43bc8291cc767a5a737b4c3c17a001c6
sharmaryan <panditaryan.as@gmail.com>
2025-01-04 16:08:22 +0530

    feat : add link to open blog add page

--- frontend/app/components/NavBar/NavBar.tsx
+++ frontend/app/components/NavBar/NavBar.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import Link from "next/link";
+
 export const NavBar = () => {
   return (
-    <header className="border-b border-gray-200 p-2">
+    <header className="flex items-center border-b border-gray-200 p-2">
       <h1 className="text-2xl font-bold">Medium</h1>
+      <Link href={'/new'} className="ml-auto border-2 border-secondary px-4 py-2 rounded-xl">Create Post</Link>
     </header>
   );
 };
