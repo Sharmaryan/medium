597c102960be90838380ea4abd9eeb7a8372728c
sharmaryan <panditaryan.as@gmail.com>
2025-01-10 22:13:19 +0530

    refactor : removed unnecessary code

--- frontend/app/new/page.tsx
+++ frontend/app/new/page.tsx
@@ -1,14 +1,10 @@
 import { BlogForm } from "../components/BlogForm/BlogForm";
-import { getServerSession } from "next-auth";
-import { NEXT_AUTH } from "../lib/auth";
 
 export default async function New() {
-  const session = await getServerSession(NEXT_AUTH);
-
   return (
     <div className="px-2">
       <h1>Create a Post</h1>
-      <BlogForm session={session} />
+      <BlogForm />
     </div>
   );
 }
