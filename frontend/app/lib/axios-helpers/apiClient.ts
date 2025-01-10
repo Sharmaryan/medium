0d5451d71312036fe88d24d3f12c9c8c8ce22a8d
sharmaryan <panditaryan.as@gmail.com>
2025-01-10 22:12:41 +0530

    refactor : updated response type in api client

--- frontend/app/lib/axios-helpers/apiClient.ts
+++ frontend/app/lib/axios-helpers/apiClient.ts
@@ -1,25 +1,26 @@
+import { AxiosResponse } from "axios";
 import createAxiosInstance from "./axiosInstance";
 
-export const getReq = async <T>(url: string, params?: Record<string, any>, clientToken?: string): Promise<T> => {
+export const getReq = async <T>(url: string, params?: Record<string, any>, clientToken?: string): Promise<AxiosResponse<T>> => {
   const apiClient = await createAxiosInstance(clientToken);
   const response = await apiClient.get<T>(url, { params });
-  return response.data;
+  return response;
 };
 
-export const postReq = async <T>(url: string, data?: Record<string, any>, clientToken?: string): Promise<T> => {
+export const postReq = async <T>(url: string, data?: Record<string, any>, clientToken?: string): Promise<AxiosResponse<T>> => {
   const apiClient = await createAxiosInstance(clientToken);
   const response = await apiClient.post<T>(url, data);
-  return response.data;
+  return response;
 };
 
-export const putReq = async <T>(url: string, data?: Record<string, any>, clientToken?: string): Promise<T> => {
+export const putReq = async <T>(url: string, data?: Record<string, any>, clientToken?: string): Promise<AxiosResponse<T>> => {
   const apiClient = await createAxiosInstance(clientToken);
   const response = await apiClient.put<T>(url, data);
-  return response.data;
+  return response;
 };
 
-export const delReq = async <T>(url: string, clientToken?: string): Promise<T> => {
+export const delReq = async <T>(url: string, clientToken?: string): Promise<AxiosResponse<T>> => {
   const apiClient = await createAxiosInstance(clientToken);
   const response = await apiClient.delete<T>(url);
-  return response.data;
+  return response;
 };
