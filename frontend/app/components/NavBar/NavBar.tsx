b8ec77af294befb4df786ca6deb014082ba81c17
sharmaryan <panditaryan.as@gmail.com>
2025-01-10 16:03:21 +0530

    fix : showing logout btn when user is logged in

--- frontend/app/components/NavBar/NavBar.tsx
+++ frontend/app/components/NavBar/NavBar.tsx
@@ -1,16 +1,25 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { Button } from "../Button/Button";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 export const NavBar = () => {
+  const session = useSession();
   return (
     <header className="flex items-center border-b border-gray-200 p-2">
       <h1 className="text-2xl font-bold">Medium</h1>
       <Button fill="outline" className="ml-auto">
         <Link href="/new">Create Post</Link>
       </Button>
-      <Button fill="solid" onClick={() => signOut({callbackUrl:'/signin'})} className="ml-5">Logout</Button>
+      {session.data?.user.token && (
+        <Button
+          fill="solid"
+          onClick={() => signOut({ callbackUrl: "/signin" })}
+          className="ml-5"
+        >
+          Logout
+        </Button>
+      )}
     </header>
   );
 };
