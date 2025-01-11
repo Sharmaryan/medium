4935ec589814d4a7bb43b99ae4c8ff7e5014f25e
sharmaryan <panditaryan.as@gmail.com>
2025-01-11 19:54:14 +0530

    feat : created toast provider and added it in provider file

---
+++ frontend/app/context/toast-provider.tsx
@@ -0,0 +1,55 @@
+import { Status, ToastTypes } from "../components/Toast/Toast.types";
+import { createContext, useContext, useMemo, useState } from "react";
+import { Toast } from "../components/Toast/Toast";
+
+type ToastMessage = Omit<ToastTypes, "onClose" | "duration">;
+
+type ToastContextType = {
+  addToast: (type: Status, message: string) => void;
+};
+
+const ToastContext = createContext<ToastContextType | null>(null);
+
+export const ToastProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [toasts, setToasts] = useState<ToastMessage[]>([]);
+
+  const addToast = useMemo(
+    () => (type: Status, message: string) => {
+      const id = Date.now().toString();
+      setToasts((prev) => [...prev, { id, type, message }]);
+    },
+    []
+  );
+
+  const removeToast = (id: string) => {
+    setToasts((prev) => prev.filter((toast) => toast.id !== id));
+  };
+
+  const value = useMemo(() => ({ addToast }), [addToast]);
+  return (
+    <ToastContext.Provider value={value}>
+      {children}
+      {toasts.map((toast) => (
+        <div key={toast.id}>
+          <Toast
+            key={toast.id}
+            id={toast.id}
+            type={toast.type}
+            message={toast.message}
+            onClose={removeToast}
+          />
+        </div>
+      ))}
+    </ToastContext.Provider>
+  );
+};
+
+export const useToast = () => {
+  const context = useContext(ToastContext);
+  if (!context) {
+    throw new Error("useToast must be used within a ToastProvider");
+  }
+  return context;
+};
--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,11 +1,12 @@
-'use client';
-import React from 'react';
-import { SessionProvider } from 'next-auth/react';
+"use client";
+import React from "react";
+import { SessionProvider } from "next-auth/react";
+import { ToastProvider } from "./context/toast-provider";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider>
-      {children}
+      <ToastProvider>{children}</ToastProvider>
     </SessionProvider>
   );
 };
