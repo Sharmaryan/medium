866f603ab69b8e315cf83420bfdc525f90979100
sharmaryan <panditaryan.as@gmail.com>
2025-01-12 15:45:00 +0530

    feat:update loading state and loader in <Button />

--- frontend/app/components/Button/Button.tsx
+++ frontend/app/components/Button/Button.tsx
@@ -2,18 +2,23 @@
 import React from "react";
 import { ButtonProps } from "./Button.types";
 import classNames from "classnames";
+import { Loader } from "../Loader/Loader";
+import { Size } from "../Loader/Loader.types";
 
-export const Button = ({ children, fill, className, ...rest }: ButtonProps) => {
+export const Button = ({ children, fill, className, isLoading=false, ...rest }: ButtonProps) => {
   return (
     <button
       {...rest}
-      className={classNames("rounded-md p-2 w-fit",  {
-        "bg-secondary text-primary": fill === "solid",
+      className={classNames("flex gap-2 justify-center rounded-md p-2 w-fit",  {
+        "bg-secondary text-primary": fill === "solid" && !isLoading,
         "border-secondary border": fill === "outline",
         "rounded-none p-0": fill === "clear",
+        "bg-disable text-[#ABABAB]": isLoading
       },className)}
+      disabled={isLoading}
     >
       {children}{fill === "clear" && "   >"}
+      {isLoading && <Loader size={Size.Default}/>}
     </button>
   );
 };
--- frontend/app/components/Button/Button.types.ts
+++ frontend/app/components/Button/Button.types.ts
@@ -3,4 +3,5 @@ import { ButtonHTMLAttributes, ReactNode } from "react";
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children : ReactNode
     fill: 'outline' | 'solid' | 'clear'
+    isLoading? : boolean
 }
