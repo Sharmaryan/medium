78fedc8c317d97ea87661e80d70e216023c20d5a
sharmaryan <panditaryan.as@gmail.com>
2025-01-13 20:33:02 +0530

    refactor : loggingInUser utils

--- frontend/app/lib/constants/auth/auth.ts
+++ frontend/app/lib/constants/auth/auth.ts
@@ -2,12 +2,7 @@ import { signIn } from "next-auth/react";
 import { SignInPayload } from "./auth.types";
 
 export const loggingInUser = async (payload: SignInPayload) => {
-    try {
-        await signIn("credentials", payload);
-        return true
-    } catch (err) {
-        return false
-    }
+    return await signIn("credentials", payload);
 };
 
-export const USER_ROUTE = `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/user`
+export const USER_ROUTE = "/api/v1/user"
