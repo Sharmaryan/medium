d39d6f816556a406ec111b60681d56a962081549
sharmaryan <panditaryan.as@gmail.com>
2025-02-01 10:08:42 +0530

    chore : config redux

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -2,11 +2,14 @@
 import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { ToastProvider } from "./context/toast-provider";
+import StoreProvider from "./context/store-provider";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SessionProvider>
-      <ToastProvider>{children}</ToastProvider>
-    </SessionProvider>
+    <StoreProvider>
+      <SessionProvider>
+        <ToastProvider>{children}</ToastProvider>
+      </SessionProvider>
+    </StoreProvider>
   );
 };
